e602660f225a36210574b7efcf0266b83a093a65
Conner Van Loan <cdvanloan@gmail.com>
2023-01-08 14:43:55 -0700

    Adds #initialize test for Auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,12 @@ require './lib/attendee'
 RSpec.describe "Item" do
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') }
-  let(:attendee) { attendee.new(name: "Megan", budget: '$50') } 
+  let(:attendee) { Attendee.new(name: "Megan", budget: '$50') } 
+  let(:auction) { Auction.new }
   
+  describe '#initialize' do
+    it 'exists' do
+      expect(auction).to be_a(Auction)
+    end
+  end
 end
