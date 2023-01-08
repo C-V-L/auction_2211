add7ed85e1d4711af3fd57b7144ab66e9cbab4a6
Conner Van Loan <cdvanloan@gmail.com>
2023-01-08 15:01:58 -0700

    Adds #add_bid test and method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,32 +5,50 @@ require './lib/attendee'
 RSpec.describe "Item" do
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') }
-  let(:attendee) { Attendee.new(name: "Megan", budget: '$50') } 
+  let(:attendee1) { Attendee.new(name: "Megan", budget: '$50') } 
   let(:auction) { Auction.new }
   
-  describe '#initialize' do
-    it 'exists' do
-      expect(auction).to be_a(Auction)
+  describe 'IT1' do
+    describe '#initialize' do
+      it 'exists' do
+        expect(auction).to be_a(Auction)
+      end
+
+      it 'starts with empty items array' do
+        expect(auction.items).to eq([])
+      end
     end
 
-    it 'starts with empty items array' do
-      expect(auction.items).to eq([])
+    describe '#add_item' do
+      it 'can add an atem to the auction' do
+        auction.add_item(item1)
+        auction.add_item(item2)
+        expect(auction.items).to eq([item1, item2])
+      end
+    end
+
+    describe '#item_names' do
+      it 'can return an array with the names of all items at auction' do
+        auction.add_item(item1)
+        auction.add_item(item2)
+        expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+      end
     end
   end
-
-  describe '#add_item' do
-    it 'can add an atem to the auction' do
+  
+  describe 'IT2' do
+    before(:each) do
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+      attendee3 = Attendee.new(name: 'Mike', budget: '$100')
       auction.add_item(item1)
       auction.add_item(item2)
-      expect(auction.items).to eq([item1, item2])
-    end
-  end
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
 
-  describe '#item_names' do
-    it 'can return an array with the names of all items at auction' do
-      auction.add_item(item1)
-      auction.add_item(item2)
-      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,9 +9,17 @@ RSpec.describe "Item" do
       expect(item1).to be_a(Item)
     end
 
-    it 'has a name' do
+    it 'has a name and starts with no bids' do
       expect(item1.name).to eq('Chalkware Piggy Bank')
       expect(item2.name).to eq('Bamboo Picture Frame')
+      expect(item1.bids).to eq({})
+    end
+  end
+
+  describe '#add_bid' do
+    it 'can add a bid to an item' do
+      item1.add_bid("attendee", 20)
+      expect(item1.bids).to eq({"attendee" => 20})
     end
   end
 end
