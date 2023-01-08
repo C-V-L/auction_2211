84bfbdadf62f96ad298658e793b79c151c132e79
Conner Van Loan <cdvanloan@gmail.com>
2023-01-08 14:50:27 -0700

    Adds #item_names method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -28,6 +28,8 @@ RSpec.describe "Item" do
 
   describe '#item_names' do
     it 'can return an array with the names of all items at auction' do
+      auction.add_item(item1)
+      auction.add_item(item2)
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
